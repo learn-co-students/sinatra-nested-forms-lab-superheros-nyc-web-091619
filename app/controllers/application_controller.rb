3488370ed29d26b304af525748f0be84665f29d7
H. Turcan Cicek <htcicek@users.noreply.github.com>
2019-10-08 08:59:46 -0400

    met all specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,24 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+    get '/' do
+        erb :super_hero
+    end
+
+    post '/team' do
+        @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+        
+        params[:team][:heroes].each do |attributes|
+            name = attributes[:name]
+            power = attributes[:power]
+            bio = attributes[:biography]
+
+            Hero.new(name: name, power: power, biography: bio)
+        end
+
+        @heroes = Hero.all
 
 
+        erb :team
+    end
 end
